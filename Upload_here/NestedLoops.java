ab168374b1e805667c473b56cba332b15752398c
Essa Imhmed <imhmede@Essas-MBP.home>
2025-03-28 09:11:36 -0600

    Code Quality

--- Upload_here/NestedLoops.java
+++ Upload_here/NestedLoops.java
@@ -1,26 +1,30 @@
-public class NestedLoops {
-    public static void main(String[] args) {
-        String line = "Message me when you get my Message";
-        String substring = "age";
-        int occurrence = 0;
-        for (int i = 0; i < line.length(); i++) {
-            String charFromLine = Character.toString(line.charAt(i));
-            if (Character.toString(substring.charAt(0)).equalsIgnoreCase(charFromLine)) {
-                int j = 0;
-                for (j = 0; j < substring.length(); j++) {
-                    //i += j;
-                    charFromLine = Character.toString(line.charAt(i));
-                    if (!Character.toString(substring.charAt(j)).equalsIgnoreCase(charFromLine)) {
-                        break;
-                    }
-                    i += 1;
-                }
-                if (j == substring.length()) {
-                    occurrence += 1;
-                }
-                
-            }
-        }
-        System.out.println(occurrence);
-    }
-}
+ import java.util.Scanner;
+ /**
+  * @author Essa Imhmed
+  */
+
+//This program reads, scales, and reverses a sequence of numbers.
+public class Main
+{public static void main(String[]args )
+   {        
+      double[] a=read_inputs(5);multiply(a, 10);printreversed(a);
+   }
+   // Reads a sequence of floating-point numbers.
+   // n is the number of inputs to read
+   public static double[] read_inputs(int n)
+   {
+      System.out.println("Enter "+n+" numbers: " );
+      Scanner in=new Scanner(System.in); double[] inputs=new double[n];for(int i=0;i<inputs.length;i++){inputs[i]=in.nextDouble();}return inputs;
+   }
+   //Multiplies all elements of an array by a factor.
+   public static void multiply(double[] values,double factor)
+   {
+      for(int i=0;i<values.length;i++){values[i]=values[i]*factor;}
+   }
+   //Prints an array in reverse order.
+   public static void printreversed(double[] v)
+   {// Traverse the array in reverse order, starting with the last element
+      for(int i=v.length-1;i>=0;i--)
+      {System.out.print(v[i]+" ");}
+      System.out.println();
+   }}
