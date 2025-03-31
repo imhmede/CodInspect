d2316d2fca08f1b8568f1e5dce7af3888c971146
Essa Imhmed <imhmede@Essas-MBP.home>
2025-03-31 11:52:43 -0600

    test

--- Upload_here/Main.java
+++ Upload_here/Main.java
@@ -1,30 +1,85 @@
- import java.util.Scanner;
- /**
-  * @author pamela garcia
-  */
+import java.util.Scanner;
+// Javadoc documentation is now available for this program.
+/**
+ * This program reads, scales, and reverses a sequence of numbers.
+ * @author Lauren White
+ * */
+public class NumberModifier
+{ 
+    /*
+    * General fixes: Corrected spacing. 
+    * Javadoc documentation is now available for every method.
+    * Fixed camel case and names for variables and methods.
+    * Moved main method to the end of the program.
+    */
 
-//This program reads, scales, and reverses a sequence of numbers.
-public class Main
-{public static void main(String[]args )
-   {        
-      double[] a=read_inputs(5);multiply(a, 10);printreversed(a);
-   }
-   // Reads a sequence of floating-point numbers.
-   // n is the number of inputs to read
-   public static double[] read_inputs(int n)
+    /**
+    * Reads a sequence of floating-point numbers.
+    * @param n is the number of inputs as a floating point number.
+    * @return inputs as a floating point number.
+    */
+   public static double[] readInputs(int n)
    {
-      System.out.println("Enter "+n+" numbers: " );
-      Scanner in=new Scanner(System.in); double[] inputs=new double[n];for(int i=0;i<inputs.length;i++){inputs[i]=in.nextDouble();}return inputs;
+        // Fixes: Corrected line separation and spacing. Closed scanner. Added comment.
+        
+        // Gets user input.
+        System.out.println("Enter " + n + " numbers: " );
+        Scanner in = new Scanner(System.in); 
+        double[] inputs = new double[n];
+        in.close();
+        
+        // Puts user input into an array.
+        for(int i = 0; i < inputs.length; i++)
+        {
+            inputs[i] = in.nextDouble();
+        }
+
+        return inputs;
    }
-   //Multiplies all elements of an array by a factor.
-   public static void multiply(double[] values,double factor)
-   {
-      for(int i=0;i<values.length;i++){values[i]=values[i]*factor;}
-   }
-   //Prints an array in reverse order.
-   public static void printreversed(double[] v)
-   {// Traverse the array in reverse order, starting with the last element
-      for(int i=v.length-1;i>=0;i--)
-      {System.out.print(v[i]+" ");}
-      System.out.println();
-   }}
+   /**
+    * Multiplies all elements of an array by a factor.
+    * @param values takes numbers from the user as a floating point number.
+    * @param factor takes the number you want to factor the values by 
+    * as a floating point number.
+    */
+    public static void multiply(double[] values, double factor)
+    {
+        // Iterates through the array and multiplies each number by factor.
+        for(int i=0;i<values.length;i++)
+        {
+            values[i] = values[i]*factor;
+        }
+    }
+    
+    // Fix: Changed variable name from v to a descriptive name.
+
+    /**
+    * This method prints an array in reverse order.
+    * @param values takes numbers from readInputs() as floating point numbers.
+    */
+    public static void printReversed(double[] values)
+    {
+        // Traverse the array in reverse order, starting with the last element.
+        for(int i=values.length-1;i>=0;i--)
+        {
+            System.out.print(values[i] + " ");
+        }
+
+        System.out.println();
+    } 
+
+   /**
+    * This is the main method.
+    * @param args takes command line arguments.
+    */
+    public static void main(String[] args) 
+    {        
+        // Fixes: Added spacing. Added a comment. Changed variable a to a fitting name.
+
+        // Reads user inputs, multiplies them, and prints the array in reverse.
+        double[] userInput = readInputs(5);
+        multiply(userInput, 10);
+        printReversed(userInput);
+    }
+} // Fix: Moved closed bracket
+  
