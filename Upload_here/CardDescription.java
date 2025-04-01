49396efd139eccab5facc0ef88e8bcdc4de2cb04
Essa Imhmed <imhmede@Essas-MBP.home>
2025-03-31 23:03:14 -0600

    test

--- Upload_here/CardDescription.java
+++ Upload_here/CardDescription.java
@@ -1,96 +1,73 @@
 package Upload_here;
 
 import java.util.Scanner;
+
 /**
- * this program reads a card description based on a notation
- * @author Dominic Mendoza
+ * This program receives two parameters describing a card rank and a card suit and returns the description
+ * while also indicating an invalid format or rank if entered.
+ * @author Jessica Provencio
  */
-public class CardDescription{
-      /**
-     * This method finds the min number in the array
-     * @param rank 
-     * @param suit
-     * @return index
+public class CardDescription {
+
+    /**
+     * Converts shorthand card notation into a full card description.
+     * @param rank the rank of the card 
+     * @param suit the suit of the card 
+     * @return full description or an invalid message
      */
-  public static String getCardDescription(String rank,String suit){
-    {
-        String rankDescription = "";
-        String suitDescription = "";
-        if(rank.equalsIgnoreCase("A")){
-          rankDescription="Ace";
-        }else if(rank.equalsIgnoreCase("J")){
-          rankDescription="Jack";
-        }else if(rank.equalsIgnoreCase("K")){
-          rankDescription="King";
-        }else if(rank.equalsIgnoreCase("Q")){
-        rankDescription="Queen";
-        }else{
-          rankDescription=rank;
+    public static String getCardDescription(String rank, String suit) {
+        String fullRank = "";
+        String fullSuit = "";
+
+        switch (rank) {
+            case "A": fullRank = "Ace"; break;
+            case "J": fullRank = "Jack"; break;
+            case "Q": fullRank = "Queen"; break;
+            case "K": fullRank = "King"; break;
+            default:
+                if (rank.matches("[2-9]|10")) {
+                    fullRank = rank;
+                } else {
+                    return "Invalid rank: " + rank;
+                }
         }
-        
-        if(suit.equalsIgnoreCase("S")){
-          suitDescription="Spades";
-        }else if(suit.equalsIgnoreCase("D")){
-          suitDescription="Diamonds";
-        }else if(suit.equalsIgnoreCase("H")){
-          suitDescription="Hearts";
-        }else if(suit.equalsIgnoreCase("C")){
-          suitDescription="Clubs";}
-        
-      
-        
-            return rankDescription +" of "+ suitDescription;
-        
-    }
-    }  
-  /**
-   * @param rank,suit
-   * This methods asks user for input notation
-   *and prints out the card description
-   */
-      
-    public static void main(String[] args){
-      Scanner in = new Scanner(System.in);
-      System.out.println("Enter card notation: ");
-    
-      String code = in.nextLine();
-      in.close();
-      String rank = Character.toString(code.charAt(0));
-      String suit = Character.toString(code.charAt(1));
-      String message= getCardDescription(rank,suit);
-      System.out.println(message);
 
+        switch (suit) {
+            case "D": fullSuit = "Diamonds"; break;
+            case "H": fullSuit = "Hearts"; break;
+            case "S": fullSuit = "Spades"; break;
+            case "C": fullSuit = "Clubs"; break;
+            default: return "Invalid suit: " + suit;
+        }
 
+        return fullRank + " of " + fullSuit;
     }
 
-  }
-  
-    
+     /**
+     * This is the main method.
+     * @param args command-line arguments 
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter card notation (ex. AC for Ace of Clubs): ");
+        String input = scanner.next().toUpperCase();
+        String rank, suit;
 
-    
-       
+        if (input.length() == 2) {
+            rank = input.substring(0, 1);
+            suit = input.substring(1, 2);
+        } else if (input.length() == 3 && input.startsWith("10")) {
+            rank = "10";
+            suit = input.substring(2);
+        } else {
+            System.out.println("Invalid input format.");
+            scanner.close();
+            return;
+        }
 
+        String description = getCardDescription(rank, suit);
+        System.out.println(description);
 
-
-
-    
-    
-
-  
-    
-        
-
-    
-
-
-        
-
-        
-
-
-    
-
-
-
-
-
+        scanner.close();
+    }
+}
