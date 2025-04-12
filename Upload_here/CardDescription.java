11090275592bd50cab6836d2171638702b4fd599
Essa Imhmed <imhmede@Essas-MBP.home>
2025-04-12 13:00:08 -0600

    test

--- Upload_here/CardDescription.java
+++ Upload_here/CardDescription.java
@@ -1,66 +1,53 @@
 /**
- * Author: Essa Imhmed
- * Date: 03/25/2025
- * Program: CardDescription
- * This program converts card shorthand notation (e.g., "Q", "S") 
- *              into full card descriptions (e.g., "Queen of Spades").
+ * @author Essa Imhmed
  */
 
- public class CardDescription {
-
-    /**
-     * Converts rank and suit into full description of a card.
-     * @param rank The shorthand card rank (e.g., "A", "2", ..., "10", "J", "Q", "K").
-     * @param suit The shorthand card suit (e.g., "D", "H", "S", "C").
-     * @return Full card description (e.g., "Queen of Spades") or error message for invalid input.
-     */
-    public static String getCardDescription(String rank, String suit) {
-        String fullRank = "";
-        String fullSuit = "";
-
-        // Determine full rank
-        if (rank.equals("A")) {
-            fullRank = "Ace";
-        } else if (rank.equals("J")) {
-            fullRank = "Jack";
-        } else if (rank.equals("Q")) {
-            fullRank = "Queen";
-        } else if (rank.equals("K")) {
-            fullRank = "King";
-        } else if (rank.matches("[2-9]|10")) {
-            fullRank = rank;
-        } else {
-            return "Invalid card rank!";
+public class CardDescription {
+    
+        public static String getCardDescription(String rank, String suit) {
+            String rankDescription = "";
+            String suitDescription = "";
+            if (rank.equals("A")) {
+                rankDescription = "Ace";
+            } else if (rank.equals("2")||(rank.equals("3") || rank.equals("4") || rank.equals("5") || rank.equals("6")
+                    || rank.equals("7") || rank.equals("8") || rank.equals("9") || rank.equals("10"))) {
+                rankDescription = rank; 
+            } else if (rank.equals("J")) {
+                rankDescription = "Jack";
+            } else if (rank.equals("Q")) {
+                rankDescription = "Queen";
+            } else if (rank.equals("K")) {
+                rankDescription = "King";
+            } else {
+                return "Invalid rank Valid ranks are A, 2-10, J, Q, K.";
+            }
+    
+            if (suit.equals("D")) {
+                suitDescription = "Diamonds";
+            } else if (suit.equals("H")) {
+                suitDescription = "Hearts";
+            } else if (suit.equals("S")) {
+                suitDescription = "Spades";
+            } else if (suit.equals("C")) {
+                suitDescription = "Clubs";
+            } else {
+                return "Invalid suit Valid suits are D, H, S, C.";
+            }
+    
+            
+            return rankDescription + " of " + suitDescription;
         }
-
-        // Determine full suit
-        if (suit.equals("D")) {
-            fullSuit = "Diamonds";
-        } else if (suit.equals("H")) {
-            fullSuit = "Hearts";
-        } else if (suit.equals("S")) {
-            fullSuit = "Spades";
-        } else if (suit.equals("C")) {
-            fullSuit = "Clubs";
-        } else {
-            return "Invalid card suit!";
+    
+        public static void main(String[] args) {
+            System.out.println(getCardDescription("Q", "S"));
+            System.out.println(getCardDescription("A", "D"));
+            System.out.println(getCardDescription("10", "H"));
+            System.out.println(getCardDescription("J", "C"));
+            System.out.println(getCardDescription("K", "S")); 
+    
+            System.out.println(getCardDescription("Z", "S")); 
+            System.out.println(getCardDescription("Q", "X"));
+            System.out.println(getCardDescription("11", "D"));
+            System.out.println(getCardDescription("A", "Z"));
         }
-
-        return fullRank + " of " + fullSuit;
     }
-
-    /**
-     * Main method to test getCardDescription with various inputs.
-     */
-    public static void main(String[] args) {
-        // Valid test cases
-        System.out.println(getCardDescription("Q", "S"));  // Queen of Spades
-        System.out.println(getCardDescription("10", "D")); // 10 of Diamonds
-        System.out.println(getCardDescription("A", "H"));  // Ace of Hearts
-        System.out.println(getCardDescription("7", "C"));  // 7 of Clubs
-
-        // Invalid test cases
-        System.out.println(getCardDescription("Z", "S"));  // Invalid card rank!
-        System.out.println(getCardDescription("5", "X"));  // Invalid card suit!
-    }
-}
