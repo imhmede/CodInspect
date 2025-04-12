0c7de7398b49b33e00d968489e9e7bb5476a3635
Essa Imhmed <imhmede@Essas-MBP.home>
2025-04-12 13:42:08 -0600

    test

--- Upload_here/CardDescription.java
+++ Upload_here/CardDescription.java
@@ -1,118 +1,150 @@
 import java.util.Scanner;
-/**
- * This program finds two parameters: card rank and card suit.
- * @author Serenity Weddle
- * @date March 27, 2025
+
+/*
+ * @Seth Weddle
+ * @3/27/2025
  */
-public class CardDescription {
-     public static String getCardDescription(String rank, String suit) {
-        // Inital values
-        String cardRank;
-        String cardSuit;
-        String cardName;
 
-        // Possible input values for the card's rank.
-        if(rank.equals("A"))
-        {
-            cardRank = "Ace";
-        }
-        else if(rank.equals("J"))
-        {
-            cardRank = "Jack";
-        }
-        else if(rank.equals("Q"))
-        {
-            cardRank = "Queen";
-        }
-        else if(rank.equals("K"))
-        {
-            cardRank = "King";
-        }
-        else if(rank.equals("2"))
-        {
-            cardRank = "2";
-        }
-        else if(rank.equals("3"))
-        {
-            cardRank = "3";
-        }
-        else if(rank.equals("4"))
-        {
-            cardRank = "4";
-        }
-        else if(rank.equals("5"))
-        {
-            cardRank = "5";
-        }
-        else if(rank.equals("6"))
-        {
-            cardRank = "6";
-        }
-        else if(rank.equals("7"))
-        {
-            cardRank = "7";
-        }
-        else if(rank.equals("8"))
-        {
-            cardRank = "8";
-        }
-        else if(rank.equals("9"))
-        {
-            cardRank = "9";
-        }
-        else if(rank.equals("10"))
-        {
-            cardRank = "10";
-        }
-        else {
-            cardRank = "Invalid Input.";
-        }
 
-        // Possible input values for the card's suit.
-        if(suit.equals("H"))
-        {
-              cardSuit = "Hearts";
-        }
-        else if(suit.equals("D"))
-        {
-              cardSuit = "Diamonds";
-        }
-        else if(suit.equals("S"))
-        {
-              cardSuit = "Spades";
-        }
-        else if(suit.equals("C"))
-        {
-              cardSuit = "Clubs";
-        }
-        else {
-              cardSuit = "Invalid Input.";
-        }
+public class cardDescription 
+{
+      /*
+       * This method will return a string that gives the card name
+       */
+      static String getCardDescription(String rank, String suit) {
+            String ranking = "";
+            String cardSuit = null;
+            String cardName = null;
+      
+            
+            if(rank.equals("Q"))
+            {
+                  ranking = "Queen";
+            }
+            else if(rank.equals("q"))
+            {
+                  ranking = "Queen";
+            }
+            else if(rank.equals("A "))
+            {
+                  ranking = "Ace";
+            }
+            else if(rank.equals("a"))
+            {
+                  ranking = "Ace";
+            }
+            else if(rank.equals("J"))
+            {
+                  ranking = "Jack";
+            }
+            else if(rank.equals("j"))
+            {
+                  ranking = "Jack";
+            }
+            else if(rank.equals("K"))
+            {
+                  ranking = "King";
+            }
+            else if(rank.equals("k"))
+            {
+                  ranking = "King";
+            }
+            else if(rank.equals("2"))
+            {
+                  ranking = "2";
+            }
+            else if(rank.equals("3"))
+            {
+                  ranking = "3";
+            }
+            else if(rank.equals("4"))
+            {
+                  ranking = "4";
+            }
+            else if(rank.equals("5"))
+            {
+                  ranking = "5";
+            }
+            else if(rank.equals("6"))
+            {
+                  ranking = "6";
+            }
+            else if(rank.equals("7"))
+            {
+                  ranking = "7";
+            }
+            else if(rank.equals("8"))
+            {
+                  ranking = "8";
+            }
+            else if(rank.equals("9"))
+            {
+                  ranking = "9";
+            }
+            else if(rank.equals("10"))
+            {
+                  ranking = "10";
+            }else{
+            ranking = "invalid";
+            }
 
-        cardName = "The card is the " + cardRank + " of " + cardSuit + "!a";
-        
-        // Returns values
-        if(cardRank.equals("Invalid Input.")){
-              return "Invalid Input.";
+            if(suit.equals("H"))
+            {
+                  cardSuit = "Hearts";
+            }
+            else if(suit.equals("h"))
+            {
+                  cardSuit = "Hearts";
+            }
+            else if(suit.equals("D"))
+            {
+                  cardSuit = "Diamonds";
+            }
+            else if(suit.equals("d"))
+            {
+                  cardSuit = "Diamonds";
+            }
+            else if(suit.equals("S"))
+            {
+                  cardSuit = "Spades";
+            }
+            else if(suit.equals("s"))
+            {
+                  cardSuit = "Spades";
+            }
+            else if(suit.equals("C"))
+            {
+                  cardSuit = "Clubs";
+            }
+            else if(suit.equals("c"))
+            {
+                  cardSuit = "Clubs";
+            }else{
+                  cardSuit = "invalid";
+            }
+            
+            cardName = "The card you have entered is the " + ranking + " of " + cardSuit;
+            
+            if(ranking.equals("invalid")){
+                  return "invalid input";
+            }else if(cardSuit.equals("invalid")){
+                  return "invalid input";
+            }else{
+                  return cardName;
+            }
         }
-        else if(cardSuit.equals("Invalid Input.")){
-              return "Invalid Input.";
-        }
-        else {
-              return cardName;
-        }
-     }
-
-     // Receives the input.
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("What is the card's rank? (Use A, 2-10, J, Q or K.)");
-        String rank = in.nextLine();
-        System.out.println("What is the card's suit? (Use D, H, S or C.)");      
-        String suit = in.nextLine();
-        String cardName = getCardDescription(rank, suit);
-        System.out.println(cardName);
-        in.close();
-
-    }
+/*
+ * This is the main method it asks for user input and in theory passes it along
+ * It then prints out the card name it recieves from the method it called or reports invalid input
+ */
+public static void main(String[] args){
+Scanner in = new Scanner(System.in);
+System.out.println("Please enter the card value (example: A for Ace, 10 for 10)");
+String rank = in.nextLine();
+System.out.println("Please enter the card suit (example: S for spades, d for diamonds)");      
+String suit = in.nextLine();
+String cardName = getCardDescription(rank, suit);
+System.out.println(cardName);
+in.close();
+}
 }
