b711ec8b1a0c9f91aa172bb31f8d49ec0b1a24f7
Essa Imhmed <imhmede@Essas-MBP.home>
2025-04-12 12:55:49 -0600

    test

--- Upload_here/CardDescription.java
+++ Upload_here/CardDescription.java
@@ -1,38 +1,66 @@
-import java.util.Scanner;
 /**
- * @author Essa Imhmed
+ * Author: Essa Imhmed
+ * Date: 03/25/2025
+ * Program: CardDescription
+ * This program converts card shorthand notation (e.g., "Q", "S") 
+ *              into full card descriptions (e.g., "Queen of Spades").
  */
 
- // elliskooper
-public class CardDescription {
+ public class CardDescription {
+
+    /**
+     * Converts rank and suit into full description of a card.
+     * @param rank The shorthand card rank (e.g., "A", "2", ..., "10", "J", "Q", "K").
+     * @param suit The shorthand card suit (e.g., "D", "H", "S", "C").
+     * @return Full card description (e.g., "Queen of Spades") or error message for invalid input.
+     */
     public static String getCardDescription(String rank, String suit) {
-        String fullRank = switch (rank) {
-            case "A" -> "Ace";
-            case "J" -> "Jack";
-            case "Q" -> "Queen";
-            case "K" -> "King";
-            case "2", "3", "4", "5", "6", "7", "8", "9", "10" -> rank;
-            default -> null;
-        };
+        String fullRank = "";
+        String fullSuit = "";
 
-        String fullSuit = switch (suit) {
-            case "D" -> "Diamonds";
-            case "H" -> "Hearts";
-            case "S" -> "Spades";
-            case "C" -> "Clubs";
-            default -> null;
-        };
+        // Determine full rank
+        if (rank.equals("A")) {
+            fullRank = "Ace";
+        } else if (rank.equals("J")) {
+            fullRank = "Jack";
+        } else if (rank.equals("Q")) {
+            fullRank = "Queen";
+        } else if (rank.equals("K")) {
+            fullRank = "King";
+        } else if (rank.matches("[2-9]|10")) {
+            fullRank = rank;
+        } else {
+            return "Invalid card rank!";
+        }
 
-        return (fullRank != null && fullSuit != null) ? fullRank + " of " + fullSuit : "Invalid card";
+        // Determine full suit
+        if (suit.equals("D")) {
+            fullSuit = "Diamonds";
+        } else if (suit.equals("H")) {
+            fullSuit = "Hearts";
+        } else if (suit.equals("S")) {
+            fullSuit = "Spades";
+        } else if (suit.equals("C")) {
+            fullSuit = "Clubs";
+        } else {
+            return "Invalid card suit!";
+        }
+
+        return fullRank + " of " + fullSuit;
     }
 
+    /**
+     * Main method to test getCardDescription with various inputs.
+     */
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter card rank: ");
-        String rank = scanner.next().toUpperCase();
-        System.out.print("Enter card suit: ");
-        String suit = scanner.next().toUpperCase();
-        System.out.println("Card description: " + getCardDescription(rank, suit));
-        scanner.close();
+        // Valid test cases
+        System.out.println(getCardDescription("Q", "S"));  // Queen of Spades
+        System.out.println(getCardDescription("10", "D")); // 10 of Diamonds
+        System.out.println(getCardDescription("A", "H"));  // Ace of Hearts
+        System.out.println(getCardDescription("7", "C"));  // 7 of Clubs
+
+        // Invalid test cases
+        System.out.println(getCardDescription("Z", "S"));  // Invalid card rank!
+        System.out.println(getCardDescription("5", "X"));  // Invalid card suit!
     }
 }
