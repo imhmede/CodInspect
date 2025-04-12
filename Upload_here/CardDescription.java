83253185e114975f240187f8d8eebacfa91e834d
Essa Imhmed <imhmede@Essas-MBP.home>
2025-04-12 13:22:58 -0600

    test

--- Upload_here/CardDescription.java
+++ Upload_here/CardDescription.java
@@ -1,126 +1,71 @@
-//importing nessesary modules
 import java.util.Scanner;
-    public class CardDescription {
-// Driver code
-    public static void main(String[] args) {
-// Creating scanner for taking user input
-        Scanner sc=new Scanner(System.in);
-// prints greeting
-    System.out.println("Welcome!");
-    System.out.print("Enter the Card Notation: ");
-// taking user input
-        String card=sc.next();
-// iterating loop with string length
-        for(int i=0;i<card.length();i++){
-// Apply condition
-        if(card.charAt(i)=='A'){
-System.out.print("Ace");
-// condition used to print " of " 
-        if(i==0){
-        System.out.print(" of ");
-    }
-    }
-    else if(card.charAt(i)=='2'){
-        System.out.print("2");
-    if(i==0){
-System.out.print(" of ");
-}
-}
-        else if(card.charAt(i)=='3'){
-System.out.print("3");
-        if(i==0){
-System.out.print(" of ");
-}
-}
-        else if(card.charAt(i)=='4'){
-System.out.print("4");
-        if(i==0){
-System.out.print(" of ");
-}
-}
-        else if(card.charAt(i)=='5'){
-System.out.print("5");
-        if(i==0){
-System.out.print(" of ");
-}
-}
-        else if(card.charAt(i)=='6'){
-System.out.print("6");
-        if(i==0){
-System.out.print(" of ");
-}
-}
-        else if(card.charAt(i)=='7'){
-System.out.print("7");
-        if(i==0){
-System.out.print(" of ");
-}
-}
-        else if(card.charAt(i)=='8'){
-System.out.print("8");
-        if(i==0){
-System.out.print(" of ");
-}
-}
-        else if(card.charAt(i)=='9'){
-System.out.print("9");
-        if(i==0){
-System.out.print(" of ");
-}
-}
-        else if(card.charAt(i)=='1'&& card.charAt(i+1)=='0'){
-System.out.print("10");
-        if(i==0){
-System.out.print(" of ");
-}
-}
-        else if(card.charAt(i)=='J'){
-System.out.print("Jack");
-        if(i==0){
-System.out.print(" of ");
-}
-}
-        else if(card.charAt(i)=='Q'){
-System.out.print("Queen");
-        if(i==0){
-System.out.print(" of ");
-}
-}
-        else if(card.charAt(i)=='K'){
-System.out.print("King");
-        if(i==0){
-System.out.print(" of ");
-}
-}
-        else if(card.charAt(i)=='D'){
-System.out.print("Diamonds");
-        if(i==0){
-System.out.print(" of ");
-}
-}
-        else if(card.charAt(i)=='H'){
-System.out.print("Hearts");
-        if(i==0){
-System.out.print(" of ");
-}
-}
-        else if(card.charAt(i)=='S'){
-    System.out.print("Spades");
-        if(i==0){
-    System.out.print(" of ");
-}
-}
-    else if(card.charAt(i)=='C'){
-    System.out.print("Clubs");
-if(i==0){
-    System.out.print(" of ");
-}
-}
-// if characters don't match
-else {
-    System.out.print(" Invalid! ");
-}
 
-}
-}
+/**
+ * This program receives two parameters describing a card rank and a card suit and returns the description
+ * while also indicating an invalid format or rank if entered.
+ * @author Essa Imhmed
+ */
+public class CardDescription {
+
+    /**
+     * Converts shorthand card notation into a full card description.
+     * @param rank the rank of the card 
+     * @param suit the suit of the card 
+     * @return full description or an invalid message
+     */
+    public static String getCardDescription(String rank, String suit) {
+        String fullRank = "";
+        String fullSuit = "";
+
+        switch (rank) {
+            case "A": fullRank = "Ace"; break;
+            case "J": fullRank = "Jack"; break;
+            case "Q": fullRank = "Queen"; break;
+            case "K": fullRank = "King"; break;
+            default:
+                if (rank.matches("[2-9]|10")) {
+                    fullRank = rank;
+                } else {
+                    return "Invalid rank: " + rank;
+                }
+        }
+
+        switch (suit) {
+            case "D": fullSuit = "Diamonds"; break;
+            case "H": fullSuit = "Hearts"; break;
+            case "S": fullSuit = "Spades"; break;
+            case "C": fullSuit = "Clubs"; break;
+            default: return "Invalid suit: " + suit;
+        }
+
+        return fullRank + " of " + fullSuit;
+    }
+
+     /**
+     * This is the main method.
+     * @param args command-line arguments 
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter card notation (ex. AC for Ace of Clubs): ");
+        String input = scanner.next().toUpperCase();
+        String rank, suit;
+
+        if (input.length() == 2) {
+            rank = input.substring(0, 1);
+            suit = input.substring(1, 2);
+        } else if (input.length() == 3 && input.startsWith("10")) {
+            rank = "10";
+            suit = input.substring(2);
+        } else {
+            System.out.println("Invalid input format.");
+            scanner.close();
+            return;
+        }
+
+        String description = getCardDescription(rank, suit);
+        System.out.println(description);
+
+        scanner.close();
+    }
 }
