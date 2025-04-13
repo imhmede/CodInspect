ca5b81c8e868151c381bedfd1136e3b4b0ea8bbb
Ludwig Scherer <schererludwig@gmail.com>
2025-04-12 18:09:15 -0600

    test

--- Upload_here/CardDescription.java
+++ Upload_here/CardDescription.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 /**
- * @author Essa Imhmed
+ * @author Ludwig Scherer
  * Due date: 2/27/2025
  * This program converts shorthand card notation into full card descriptions.
  */
