1001a0e38f859df6525d86f64237607de419b9e3
Essa Imhmed <imhmede@Essas-MBP.home>
2025-04-26 16:57:22 -0600

    code quality analysis

---
+++ Upload_here/Monogram.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+/*
+@author David Burns
+@date February 16 2025
+* This Program derives the monogram initials of any string statement, but is specifically meant for use with names
+* Converts names into initials.
+*/
+
+public class Monogram {
+    /*
+     * This method uses an array and a for loop to retrieve the first index of each name entered by the user.
+     * @param fullname A String statement (a name) entered by the user with spaces between each name.
+     */
+    public static String getMonogram(String fullName) {
+        
+        String[] names= fullName.split(" ");
+        String monogram = "";
+
+        for (int i = 0;i<names.length;i++){
+            monogram = monogram + names[i].charAt(0);
+        }
+        return monogram;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name;
+
+        System.out.print("Enter your full name: ");
+        name = scanner.nextLine();
+        System.out.printf("The Monogram of %s is %s\n", name, getMonogram(name));
+        
+        scanner.close();
+    }
+}
